e22a7e2be15fcfa498f54cadfc17a10c4948ca0b
GeorgeCheng(Laptop) <ck40292@gmail.com>
2024-05-27 13:09:46 +0800

    feat: message item link

--- src/tests/handlers.ts
+++ src/tests/handlers.ts
@@ -5,25 +5,24 @@ import { mockGenerationArea } from './mocks/mockgenerationArea';
 import { mockGenerateAreaByTitle } from './mocks/mockgenerateAreaByTitle';
 
 export const handlers = [
-  http.get(baseUrl + '/api/Resume/6cafb84d-0a6b-41fa-b306-fb356f2af502/Relative', () => {
-    return new Response(JSON.stringify(mockRecruitments));
-  }),
-  http.get(baseUrl + '/api/Recruitment', () => {
-    return new Response(JSON.stringify(mockRecruitments));
-  }),
-
+  // http.get(baseUrl + '/api/Resume/6cafb84d-0a6b-41fa-b306-fb356f2af502/Relative', () => {
+  //   return new Response(JSON.stringify(mockRecruitments));
+  // }),
+  // http.get(baseUrl + '/api/Recruitment', () => {
+  //   return new Response(JSON.stringify(mockRecruitments));
+  // }),
   // http.get(baseUrl + '/api/Tag?typeId=4', () => {
   //   return new Response(JSON.stringify(generateTags));
   // }),
   // http.post(baseUrl + '/api/Area/Generation/Title', () => {
   //   return new Response(JSON.stringify(generateArea));
   // }),
-  http.post(baseUrl + '/api/Area/Generation', () => {
-    return new Response(JSON.stringify(mockGenerationArea));
-  }),
-  http.post(baseUrl + '/api/Area/Generation/Title', () => {
-    return new Response(JSON.stringify(mockGenerateAreaByTitle));
-  }),
+  // http.post(baseUrl + '/api/Area/Generation', () => {
+  //   return new Response(JSON.stringify(mockGenerationArea));
+  // }),
+  // http.post(baseUrl + '/api/Area/Generation/Title', () => {
+  //   return new Response(JSON.stringify(mockGenerateAreaByTitle));
+  // }),
   // http.get(baseUrl + '/api/Chat/Groups/', () => {
   //   return new Response(JSON.stringify(generateChatGroups));
   // }),
